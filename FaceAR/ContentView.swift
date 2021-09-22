e41728122b0346bbdcc34ebc137fbc9d4214cf90
Yuriy Prosvirnin <prosvirniny@gmail.com>
2021-09-22 23:28:11 +0300

    + 5 types of goggles

--- FaceAR/ContentView.swift
+++ FaceAR/ContentView.swift
@@ -4,7 +4,7 @@
 //
 //  Created by YURY PROSVIRNIN on 22.09.2021.
 //
-
+import ARKit
 import SwiftUI
 import RealityKit
 
@@ -20,11 +20,21 @@ struct ARViewContainer: UIViewRepresentable {
         
         let arView = ARView(frame: .zero)
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        //Checking face tracking is supported
+        guard ARFaceTrackingConfiguration.isSupported else {
+            return arView
+        }
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        let configuration = ARFaceTrackingConfiguration()
+        configuration.isLightEstimationEnabled = true
+        
+        arView.session.run(configuration, options: [])
+        
+        let anchor = AnchorEntity(.face)
+        arView.scene.anchors.append(anchor)
+        
+        arView.setupGestures()
+        arView.addGoggles()
         
         return arView
         
@@ -32,6 +42,51 @@ struct ARViewContainer: UIViewRepresentable {
     
     func updateUIView(_ uiView: ARView, context: Context) {}
     
+}
+
+extension ARView{
+    //adds tap gesture
+    func setupGestures() {
+        let gesture = UITapGestureRecognizer()
+        gesture.addTarget(self, action: #selector(self.detectSwipeGesture(sender:)))
+        self.addGestureRecognizer(gesture)
+    }
+    
+    @objc func detectSwipeGesture(sender: UITapGestureRecognizer) {
+        self.addGoggles()
+    }
+    
+    //clears existing entity and adds a random one
+    func addGoggles()  {
+        
+        guard let anchor = self.scene.anchors.first else { return }
+            
+        let index = [1,2,3,4,5].randomElement()!
+        guard let googles = loadFoodGoggles(index) else { return }
+        
+        anchor.children.removeAll()
+        anchor.addChild(googles)
+
+        }
+    }
+    
+    //loads entities from realityKit file
+    func loadFoodGoggles(_ index: Int) -> Entity? {
+        switch index {
+        case 1:
+            return try? Goggles.loadFoodGoggles1()
+        case 2:
+            return try? Goggles.loadFoodGoggles2()
+        case 3:
+            return try? Goggles.loadFoodGoggles3()
+        case 4:
+            return try? Goggles.loadFoodGoggles4()
+        case 5:
+            return try? Goggles.loadFoodGoggles5()
+        default:
+            return nil
+
+    }
 }
 
 #if DEBUG
